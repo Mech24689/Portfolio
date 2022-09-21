83401a5cafe3947da9f6a4f203defb3a977ccce2
Mech24689 <gabrieltrem102030@gmail.com>
2022-09-20 23:08:06 -0300

    cor do scroolbar, efeito de digitação no texto do sobre mim

---
+++ js/BlinkText.js
@@ -0,0 +1,21 @@
+function typeWriter(elemento) {
+    let textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+  }
+
+  // Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
+  // function typeWriter(elemento) {
+  //   const textoArray = elemento.innerHTML.split('');
+  //   elemento.innerHTML = '';
+  //   for(let i = 0; i < textoArray.length; i++) {
+  //     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+  //   }
+  // }
+
+  const titulo = document.querySelector('h1');
+  const texto = document.querySelector('p')
+
+  typeWriter(texto)
